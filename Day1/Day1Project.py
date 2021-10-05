9ba65fe4141f305359c9e36687e67a077296860c
Steven Brehmer <srbrehmer94@gmail.com>
2021-10-05 06:47:00 -0400

    Finished Day 1

---
+++ Day1/Day1Project.py
@@ -0,0 +1,6 @@
+#i should just do these projects instead of the work building to them from now on...
+print("Band Name Generator\n")
+city = input("What City did you grow up?\n")
+pet = input("What is your pets name?\n")
+band_name = city + " " + pet
+print("Band Name: " + band_name)
